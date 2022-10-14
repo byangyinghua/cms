5aa925403423cfe176d0cc505391863a492fb008
byangyinghua <shyangyinghua@outlook.com>
2022-10-14 15:56:57 +0800

    Merge remote-tracking branch 'origin/my' into my
    Signed-off-by: byangyinghua <shyangyinghua@outlook.com>
    
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/netsdk/demo/module/FaceRecognitionModule.java
    
    Signed-off-by: byangyinghua <shyangyinghua@outlook.com>

--- src/main/java/com/netsdk/demo/module/FaceRecognitionModule.java
+++ src/main/java/com/netsdk/demo/module/FaceRecognitionModule.java
@@ -1,17 +1,16 @@
 package com.netsdk.demo.module;
 
+import com.netsdk.lib.NativeString;
+import com.netsdk.lib.NetSDKLib;
+import com.netsdk.lib.NetSDKLib.*;
+import com.netsdk.lib.ToolKits;
+import com.sun.jna.Memory;
+import com.sun.jna.Pointer;
+
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
-import com.netsdk.lib.NativeString;
-import com.netsdk.lib.NetSDKLib;
-import com.netsdk.lib.ToolKits;
-import com.netsdk.lib.NetSDKLib.*;
-
-import com.sun.jna.Memory;
-import com.sun.jna.Pointer;
 
 /**
  * IVSS 和 IPC-FR 人脸功能接口实现, IPC-FD只支持人脸检测事件
@@ -34,8 +33,8 @@ public class FaceRecognitionModule {
 	public static LLong realLoadPicture(int channel, fAnalyzerDataCallBack callback) {
 		int bNeedPicture = 1; // 是否需要图片
 	
-		LLong m_hAttachHandle =  LoginModule.netsdk.CLIENT_RealLoadPictureEx(LoginModule.m_hLoginHandle, channel, 
-									NetSDKLib.EVENT_IVS_ALL, bNeedPicture, callback, null, null);
+		LLong m_hAttachHandle =  LoginModule.netsdk.CLIENT_RealLoadPictureEx(LoginModule.m_hLoginHandle, channel,
+				NetSDKLib.EVENT_IVS_ALL, bNeedPicture, callback, null, null);
         if(m_hAttachHandle.longValue() == 0) {
         	System.err.println("CLIENT_RealLoadPictureEx Failed, Error:" + ToolKits.getErrorCodePrint());
         } else {
@@ -753,9 +752,9 @@ public class FaceRecognitionModule {
 
 	/**
 	 * 显示/关闭规则库
-	 * @param RealPlayHandle  实时预览
+	 * @param m_hRealPlayHandle  实时预览
 	 * @param bTrue    1-打开, 0-关闭
-	 * @return
+
 	 */
 	public static void renderPrivateData(LLong m_hRealPlayHandle, int bTrue) {
 		if(m_hRealPlayHandle.longValue() != 0) {
