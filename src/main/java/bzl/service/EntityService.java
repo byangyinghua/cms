f40c624dfed368803adba66adcb6eec51f88f9b6
byangyinghua <Administrator@YANG>
2023-02-01 11:29:50 +0800

    Signed-off-by: byangyinghua <Administrator@YANG>

---
+++ src/main/java/bzl/service/EntityService.java
@@ -0,0 +1,29 @@
+package bzl.service;
+
+import utils.ResultModel;
+
+import java.util.List;
+import java.util.Map;
+
+public interface EntityService<T> {
+  
+
+    List<?> select(String classname,String namespace,String id,Map<String,Object> map);
+    List<?> select(String namespace, String id, Object object);
+    int insert(String classname,String id,T object);
+    int update(String classname,String id,T object);
+    int delete(String classname,String id,T object);
+  
+    int doBatch(String tablename,String operator,List<T> list);
+    int doBatch(String tablename[], String operator[],List<ResultModel> list);
+    int doThing(String tablename);
+   
+//    Object getJSONObject(String classname,String tablename,String id,HttpServletRequest request);
+//    List<T> getList(String classname,String tablename, String id,HttpServletRequest request);
+//	JSONObject getObjByPro(String classname,String id,String procname,HttpServletRequest request);
+//	Object getJSONObjectByPage(String string, String viewname, String string2,HttpServletRequest request);
+//	JSONObject getObjByPro(String procname,HttpServletRequest request);
+//	Object getPageJSONObjectByMybatis(String classname,String tablename, String id,HttpServletRequest request);
+//	JSONObject getObjByProJson(String procname, HttpServletRequest request);
+//	JSONObject getObjByMap(String procname,Map<String,Object> map);
+}
---
+++ src/test/java/com/github/sarxos/webcam/ds/buildin/natives/DeviceTest.java
@@ -0,0 +1,10 @@
+package com.github.sarxos.webcam.ds.buildin.natives;
+
+import junit.framework.TestCase;
+
+public class DeviceTest extends TestCase {
+
+    public void testName() {
+
+    }
+}
